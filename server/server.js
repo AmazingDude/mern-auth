cbff061f7d55db7a8127591a629468adf9c02f47
amazingdude <gamerskull007@gmail.com>
2025-08-19 12:30:13 +0500

    fix: configure CORS to allow frontend origin with credentials

--- server/server.js
+++ server/server.js
@@ -14,7 +14,16 @@ const allowedOrigins = ['https://mern-auth-chi-dun.vercel.app']
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({ origin: allowedOrigins, credentials: true }));
+app.use(cors({
+    origin: function (origin, callback) {
+        if (!origin || allowedOrigins.includes(origin)) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+}));
 
 
 // API Endpoints
